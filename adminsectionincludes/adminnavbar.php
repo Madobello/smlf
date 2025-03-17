64683edddc6e4a299bd20754511d2cc71a284ac4
Idt Danny <idtdanny@gmail.com>
2025-03-17 23:25:23 +0200

    user page fixed - select before proceed

--- adminsectionincludes/adminnavbar.php
+++ adminsectionincludes/adminnavbar.php
@@ -1,50 +1,40 @@
 <nav class="navbar bgcolor">
-<div class="container">
-<div class="navbar-header ">
-<button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
-<span class="icon-bar"></span>
-<span class="icon-bar"></span>
-<span class="icon-bar"></span> 
-</button>
-</div>
+	<div class="container">
+		<div class="navbar-header ">
+			<button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+				<span class="icon-bar"></span>
+				<span class="icon-bar"></span>
+				<span class="icon-bar"></span> 
+			</button>
+		</div>
 
-<div class="collapse navbar-collapse " id="myNavbar">
-	<?php
-$sql = "SELECT * FROM systeminfo ORDER BY id ASC LIMIT 1";
-$result = $conn->query($sql);
-if ($result->num_rows > 0) 
-{
-while ($row = $result->fetch_assoc()) 
-{
-?>
-<a  href="dashboard.php"><img class="img-rounded" src="systeminfouploads/<?php echo $row['logo']; ?>" alt="Current Logo" style="max-width: 100px;"></a>
-<?php
-}
-}
-else
-{
-?>
-<a  href="dashboard.php">WCRA</a>
-<?php
-}
-?>
-<ul class="nav navbar-nav">
-<li><a href="index.php#footer"><div id="google_translate_element"></div></a></li>
-<li><a href="dashboard.php">HOME</a></li>
-<li><a href="messagepage.php">MESSAGE</a></li>
-<li><a href="garageservicepage.php">SERVICES</a></li> 
-<?php
-if(!isset($_SESSION['garagename']))
-{
-?>
-<li><a href="systeminfopage.php">SYSTEM</a></li>
-<li><a href="garagepostpage.php">GARAGE</a></li> 
-<li><a href="registeredusers.php">ACCOUNT</a></li>
-<?php
-}
-?>
-<li><a href="logout.php">LOGOUT</a></li> </ul>
-</div>
-</div>
+		<div class="collapse navbar-collapse " id="myNavbar">
+			<?php 
+				$sql = "SELECT * FROM `systeminfo` ORDER BY id ASC LIMIT 1";
+				$result = $conn->query($sql);
+				if ($result->num_rows > 0) {
+					while ($row = $result->fetch_assoc()) {
+			?>
+			<a  href="dashboard.php"><img class="img-rounded" src="systeminfouploads/<?php echo $row['logo']; ?>" alt="Current Logo" style="max-width: 100px;"></a>
+			<?php } } else { ?>
+			
+			<a  href="dashboard.php">WCRA</a>
+			
+			<?php } ?>
+			
+			<ul class="nav navbar-nav">
+				<li><a href="index.php#footer"><div id="google_translate_element"></div></a></li>
+				<li><a href="dashboard.php">HOME</a></li>
+				<li><a href="messagepage.php">MESSAGE</a></li>
+				<li><a href="garageservicepage.php">SERVICES</a></li>
+				<?php if(!isset($_SESSION['garagename'])) { ?>
+				<li><a href="systeminfopage.php">SYSTEM</a></li>
+				<li><a href="garagepostpage.php">GARAGE</a></li> 
+				<li><a href="registeredusers.php">ACCOUNT</a></li>
+				<?php } ?>
+				<li><a href="logout.php">LOGOUT</a></li>
+			</ul>
+		</div>
+	</div>
 </nav>
 
--- userinfo.php
+++ userinfo.php
@@ -1,49 +1,34 @@
-<?php
-include 'phpincludes/startsession.php';
-?>
+<?php include 'phpincludes/startsession.php'; ?>
+
 <html lang="en">
-<?php
-include 'phpincludes/dbconnection.php';
-?>
-<?php
-include 'sectionincludes/navlink.php';
-?>
-<body>
-<div id="content-wrapper">
-<?php
-include 'sectionincludes/mainnavbar-user.php';
-?>
-<header class="header header--bg">
-<div class="container">
-<?php
-// include 'sectionincludes/herosection.php';
-?>
-</div>
-</header>
-<?php
-// include 'sectionincludes/useringo_section.php';
-?>
-<?php
-// include 'sectionincludes/aboutussection.php';
-?>
-<?php
-// include 'sectionincludes/overviewsection.php';
+
+<?php 
+    include 'phpincludes/dbconnection.php';
+    include 'sectionincludes/navlink.php';
 ?>
 
-<?php
-include 'sectionincludes/userappointsection.php';
-?>
-
-<?php
-// include 'adminsectionincludes/usermessageview.php';
-?>
-
-<?php
-include 'sectionincludes/footersection.php';
-?>
-</div>
-<?php
-include 'sectionincludes/footerjslink.php';
-?>
-</body>
+    <body>
+        <div id="content-wrapper">
+            <?php include 'sectionincludes/mainnavbar-user.php'; ?>
+            <header class="header header--bg">
+                <div class="container">
+                    <?php // include 'sectionincludes/herosection.php'; ?>
+                </div>
+            </header>
+            
+            <?php
+                // include 'sectionincludes/useringo_section.php';
+                // include 'sectionincludes/aboutussection.php';
+                // include 'sectionincludes/overviewsection.php';
+                
+                include 'sectionincludes/userappointsection.php';
+                
+                // include 'adminsectionincludes/usermessageview.php';
+                
+                include 'sectionincludes/footersection.php';
+            ?>
+        </div>
+        
+        <?php include 'sectionincludes/footerjslink.php'; ?>
+    </body>
 </html>
